f81e403851d7156a8d823bed89e2336933eeadb6
maartenrdijkstra <maartenrdijkstra@gmail.com>
2025-07-02 16:08:49 +0200

    nog een hoop te doen

--- src/domains/groceries/components/GroceryForm.vue
+++ src/domains/groceries/components/GroceryForm.vue
@@ -1,16 +1,36 @@
 <template>
-    <form @input='addGrocery'>
+    <form @submit=>
         <p>Voeg Item toe</p>      
-        <p>Naam</p><input v-model="product.name">
+        <p>Naam</p><input v-model="naam">
+        <p>Prijs</p><input type="number" step=".01" min="0" v-model="prijs">
+        <p>Hoeveelheid</p><input type="number"  min="0" v-model="hoeveelheid">
+        <button type="submit"></button>
     </form>
 
 </template>
 
 <script setup>
-import 
+import { addGrocery, getAllGroceries, getGroceryById } from '../store';
 
 const props = defineProps({
-  products: Array
+  getGroceryById: Object,
+  getAllGroceries: Array,
+  addGrocery: Number
 })
+
+const groceryCopy = getGroceryById(1);
+console.log(groceryCopy.value);
+const newGrocery = {};
+
+function addGroceryItem() {
+  newGrocery.id = Math.max(props.getAllGroceries.values) + 1;
+  newGrocery.name = this.naam;
+  newGrocery.productPrice = this.prijs;
+  newGrocery.productQuantity = this.hoeveelheid;
+  console.log(groceryCopy);
+  this.$emit('submit', newGrocery)
+}
+
+//TODO: maak dit werkend
 
 </script>
--- src/domains/groceries/routes.js
+++ src/domains/groceries/routes.js
@@ -1,10 +1,12 @@
 import Create from './pages/CreatePage.vue'
 import Edit from './pages/EditPage.vue'
 import Overview from './pages/OverviewPage.vue'
+import GroceryForm from './components/GroceryForm.vue'
 
 
 export const routes = [
     {path: '/', component: Overview},
     {path: '/create', component: Create},
     {path: '/edit', component: Edit},
+    {path: '/grocery', component: GroceryForm}
 ]
