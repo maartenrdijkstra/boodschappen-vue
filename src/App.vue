40fffcee42a8917dd66266ebfb8002a88113af5d
maartenrdijkstra <maartenrdijkstra@gmail.com>
2025-07-02 10:42:01 +0200

    added tofixed() methods

--- src/App.vue
+++ src/App.vue
@@ -11,15 +11,15 @@
     <tbody>
     <tr v-for="product, index in products" :key="index">
         <td> {{ product.name }}</td>
-        <td class="amount">{{ product.productPrice }}</td>
+        <td class="amount">{{ product.productPrice.toFixed(2) }} </td>
         <td class="amount"><input type="text" v-model="product.productQuantity" @input="updatePrice(index)"></td>
-        <td class="amount">{{ product.productTotalCost }}</td>
+        <td class="amount">{{ product.productTotalCost.toFixed(2)  }}</td>
     </tr>
     </tbody>
     <tfoot>
         <tr>
     <td colspan="3"><strong>Totaal</strong></td>
-   <td><strong>{{ totalCost }}</strong></td>
+   <td><strong>{{ totalCost.toFixed(2)  }}</strong></td>
     </tr>
     </tfoot>
 </table>
